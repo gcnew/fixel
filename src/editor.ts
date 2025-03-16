4f8b182f0d4613cfdbfa0975766a270c6911a60a
gcnew <marinov.ms@gmail.com>
2025-03-17 01:40:31 +0200

    Improve layouting - layout data is now dynamic, added default style

--- src/editor.ts
+++ src/editor.ts
@@ -83,7 +83,7 @@ const styleContext = {
     }
 };
 
-const styles = compileStyle<UiStyle>(styleContext, `
+const styles = compileStyle<UIStyle>(styleContext, `
 
     toolOffset = height - (toolSize + 5) * 4 - 5 - 5;
 
@@ -129,27 +129,52 @@ const styles = compileStyle<UiStyle>(styleContext, `
     }
 `);
 
+const defaultStyle: Required<UIStyle> = {
+    x: 0,
+    y: 0,
+    w: 0,
+    h: 0,
+    color: 'aqua',
+    font: '12px monospace',
+    borderW: 0,
+    borderColor: 'aqua',
+    gap: 0,
+    scroll: undefined,
+};
+
 type ImageSlice = { kind: 'image', src: string, dx: number, dy: number, w: number, h: number }
 type TextProps  = { kind: 'text', text: string, font: string, color: string }
 
 type LayoutData = {
+    style: UIStyle,
+
     x: number,
     y: number,
 
-    w?: number,
-    h?: number,
-    color?: string,
-    font?: string,
-    borderW?: string | number,
-    borderColor?: string,
-    gap?: number,
+    w: number,
+    h: number,
 
     scroll?: 'x' | 'y',
-    scrollX?: number,
-    scrollY?: number,
+    scrollX: number,
+    scrollY: number,
+
+    // Accessors
+    color: string,
+    font: string,
+    borderW: number | string,
+    borderColor: string,
+    gap: number,
 }
 
-type UiStyle = {
+type LayoutPrivate = {
+    $x: number | undefined,
+    $y: number | undefined,
+
+    $w: number | undefined,
+    $h: number | undefined,
+}
+
+type UIStyle = {
     x?: number,
     y?: number,
 
@@ -157,11 +182,11 @@ type UiStyle = {
     h?: number,
     color?: string,
     font?: string,
-    borderW?: number,
+    borderW?: number | string,
     borderColor?: string,
-    gap?: number,
 
-    scroll?: 'x' | 'y',
+    gap?: number,
+    scroll: 'x' | 'y' | undefined,
 }
 
 type UI<T> = Button<T>
@@ -339,20 +364,20 @@ function drawButton(o: Button<unknown>) {
     const ld = getOrCreateLayout(o);
 
     if (o.inner.kind === 'text') {
-        ctx.fillStyle = ld.color || 'aqua';       // TODO: [styles]
-        ctx.font = ld.font || '12px monospace';   // TODO: [styles]
+        ctx.fillStyle = ld.color;
+        ctx.font = ld.font;
 
         const dims = ctx.measureText(o.inner.text);
-        const ch = Math.round((ld.h! - dims.actualBoundingBoxAscent) / 2);   // TODO: [styles]
-        const wOffset = Math.max(ld.w! - dims.width - 4 | 0, 5);
+        const ch = Math.round((ld.h - dims.actualBoundingBoxAscent) / 2);
+        const wOffset = Math.max(ld.w - dims.width - 4 | 0, 5);
 
-        ctx.fillText(o.inner.text, ld.x + wOffset, ld.y + dims.actualBoundingBoxAscent + ch | 0, ld.w! - 9); // TODO: [styles]
+        ctx.fillText(o.inner.text, ld.x + wOffset, ld.y + dims.actualBoundingBoxAscent + ch | 0, ld.w - 9);
     }
 
     if (o.inner.kind === 'image') {
         const atlas = loadedAtlases[o.inner.src];
 
-        ctx.drawImage(atlas, o.inner.dx, o.inner.dy, o.inner.w, o.inner.h, ld.x, ld.y, ld.w!, ld.h!); // TODO: [styles]
+        ctx.drawImage(atlas, o.inner.dx, o.inner.dy, o.inner.w, o.inner.h, ld.x, ld.y, ld.w, ld.h);
     }
 
     drawBorder(ld);
@@ -361,16 +386,16 @@ function drawButton(o: Button<unknown>) {
 function drawBorder(ld: LayoutData) {
     const borderW = ld.borderW;
 
-    switch (typeof borderW) {
-        case 'undefined': {
-            return;
-        }
+    if (!borderW) {
+        return;
+    }
 
+    switch (typeof borderW) {
         case 'number': {
-            ctx.strokeStyle = ld.borderColor || 'aqua';  // TODO: [styles]
+            ctx.strokeStyle = ld.borderColor;
             ctx.lineWidth = borderW;
 
-            ctx.strokeRect(ld.x, ld.y, ld.w!, ld.h!);    // TODO: [styles]
+            ctx.strokeRect(ld.x, ld.y, ld.w, ld.h);
             return;
         }
 
@@ -378,7 +403,7 @@ function drawBorder(ld: LayoutData) {
             let parsed = borderW.split(/\s+/g)
                 .map(Number);
 
-            const strokeStyle = ld.borderColor || 'aqua';  // TODO: [styles]
+            const strokeStyle = ld.borderColor;
 
             if (parsed.length !== 2 && parsed.length !== 4) {
                 console.warn(`Bad border style: ${borderW} ${JSON.stringify(parsed)}`);
@@ -390,11 +415,11 @@ function drawBorder(ld: LayoutData) {
                 parsed = [parsed[0], parsed[1], parsed[0], parsed[1]];
             }
 
-            // TODO: [styles]
-            drawLine(ld.x, ld.y, ld.w!, 0, strokeStyle, parsed[0]);
-            drawLine(ld.x + ld.w!, ld.y, 0, ld.h!, strokeStyle, parsed[1]);
-            drawLine(ld.x, ld.y + ld.h!, ld.w!, 0, strokeStyle, parsed[2]);
-            drawLine(ld.x, ld.y, 0, ld.h!, strokeStyle, parsed[3]);
+            drawLine(ld.x, ld.y, ld.w, 0, strokeStyle, parsed[0]);
+            drawLine(ld.x + ld.w, ld.y, 0, ld.h, strokeStyle, parsed[1]);
+            drawLine(ld.x, ld.y + ld.h, ld.w, 0, strokeStyle, parsed[2]);
+            drawLine(ld.x, ld.y, 0, ld.h, strokeStyle, parsed[3]);
+            return;
         }
     }
 }
@@ -438,39 +463,39 @@ function drawOldButton(o: OldButton<unknown>) {
 
 function drawAutoContainer(o: AutoContainer<unknown>) {
     const ld = getOrCreateLayout(o);
+
     if (displayBoundingBoxes) {
         ctx.strokeStyle = 'red';
         ctx.lineWidth = 1;
-        ctx.strokeRect(ld.x! - 1, ld.y! -1, ld.w! + 1, ld.h! + 1);    // TODO: [styles]
+        ctx.strokeRect(ld.x - 1, ld.y -1, ld.w + 1, ld.h + 1);
     }
 
     if (ld.scroll) {
+        const totalHeight = o.children.reduce((acc, x) => acc + getOrCreateLayout(x).h, 0);
+        const totalWidth = o.children.reduce((acc, x) => acc + getOrCreateLayout(x).w, 0);
+
+        ld.scrollX = clamp(ld.scrollX, Math.min(ld.w, totalWidth) - totalWidth, 0);
+        ld.scrollY = clamp(ld.scrollY, Math.min(ld.h, totalHeight) - totalHeight, 0);
+
         ctx.save();
         ctx.beginPath();
-        ctx.rect(ld.x! - 1, ld.y! - 1, ld.w! + 1, ld.h! + 1);    // TODO: [styles]
+        ctx.rect(ld.x - 1, ld.y - 1, ld.w + 1, ld.h + 1);
         ctx.clip();
     }
 
-    const gap = ld.gap ?? 0;
     let dy = 0;
     let dx = 0;
-
-    const totalHeight = o.children.reduce((acc, x) => acc + getOrCreateLayout(x).h!, 0);
-    const totalWidth = o.children.reduce((acc, x) => acc + getOrCreateLayout(x).w!, 0);
-
-    ld.scrollX = clamp(ld.scrollX ?? 0, Math.min(ld.w!, totalWidth) - totalWidth, 0);
-    ld.scrollY = clamp(ld.scrollY ?? 0, Math.min(ld.h!, totalHeight) - totalHeight, 0);
 
     for (const c of o.children) {
         const childLd = getOrCreateLayout(c);
 
-        childLd.x = ld.x! + dx + ld.scrollX;  // TODO: [styles]
-        childLd.y = ld.y! + dy + ld.scrollY;  // TODO: [styles]
+        childLd.x = ld.x + dx + ld.scrollX;
+        childLd.y = ld.y + dy + ld.scrollY;
 
         if (o.mode === 'column') {
-            dy += childLd.h! + gap;    // TODO: [styles]
+            dy += childLd.h + ld.gap;
         } else {
-            dx += childLd.w! + gap;    // TODO: [styles]
+            dx += childLd.w + ld.gap;
         }
     }
 
@@ -488,18 +513,49 @@ function getOrCreateLayout(o: Button<unknown> | AutoContainer<unknown>): LayoutD
         return existing;
     }
 
-    const style = o.style !== undefined
+    const style = o.style
         ? styles[o.style]
         : undefined;
 
-    let ld = { x: 0, y: 0 };
-    if (style) {
-        ld = Object.create(style);   // TODO: [styles]
-        //ld.x = ld.y = 0;             // TODO: [styles]
-    }
-
+    const ld = createLayoutData(style);
     layoutDataCache[key] = ld;
     return ld;
+}
+
+function createLayoutData(style: UIStyle | undefined): LayoutData {
+    const res: LayoutPrivate & LayoutData = {
+        $x: undefined,
+        $y: undefined,
+        $w: undefined,
+        $h: undefined,
+
+        style: style ?? defaultStyle,
+
+        get x() { return this.$x ?? this.style.x ?? defaultStyle.x; },
+        set x(val: number) { this.$x = val; },
+
+        get y() { return this.$y ?? this.style.y ?? defaultStyle.y; },
+        set y(val: number) { this.$y = val; },
+
+        get w() { return this.$w ?? this.style.w ?? defaultStyle.w; },
+        set w(val: number) { this.$w = val; },
+
+        get h() { return this.$h ?? this.style.h ?? defaultStyle.h; },
+        set h(val: number) { this.$h = val; },
+
+        scroll: style?.scroll,
+        scrollX: 0,
+        scrollY: 0,
+
+        // Accessors
+        get color() { return style?.color || defaultStyle.color; },
+        get font() { return style?.font || defaultStyle.font; },
+        get borderW() { return style?.borderW || defaultStyle.borderW; },
+        get borderColor() { return style?.borderColor || defaultStyle.borderColor; },
+        get gap() { return style?.gap || defaultStyle.gap; },
+    };
+
+    return res;
 }
 
 function drawObjects() {
@@ -560,8 +616,8 @@ function handleClickUI(ui: UI<unknown>[]): boolean {
             case 'button': {
                 const ld = getOrCreateLayout(o);
 
-                if (mouseX >= ld.x && mouseX <= ld.x + ld.w!        // TODO: [styles]
-                    && mouseY >= ld.y && mouseY <= ld.y + ld.h!) {  // TODO: [styles]
+                if (mouseX >= ld.x && mouseX <= ld.x + ld.w
+                    && mouseY >= ld.y && mouseY <= ld.y + ld.h) {
 
                     o.onClick(o);
                     return true;
@@ -598,8 +654,8 @@ function handleScroll(ui: UI<unknown>[], deltaX: number, deltaY: number): boolea
             case 'auto-container': {
                 const ld = getOrCreateLayout(o);
 
-                if (!(mouseX >= ld.x && mouseX <= ld.x + ld.w!        // TODO: [styles]
-                    && mouseY >= ld.y && mouseY <= ld.y + ld.h!)) {   // TODO: [styles]
+                if (!(mouseX >= ld.x && mouseX <= ld.x + ld.w
+                    && mouseY >= ld.y && mouseY <= ld.y + ld.h)) {
                     return false;
                 }
 
